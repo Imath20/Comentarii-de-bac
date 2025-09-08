d3f3076f08839213bee6ff5ba25036cc93298883
FujisanRealEstate <154996184+Imath20@users.noreply.github.com>
2025-09-08 16:41:24 +0300

    added more subiecte

--- Comentarii-de-bac/src/data/subiecte.js
+++ Comentarii-de-bac/src/data/subiecte.js
@@ -27,7 +27,7 @@ Căci în fața noastră, a studenților săi extaziați de maestrul lor, nu vor
             'Explică un motiv pentru care Vasile Pârvan face referire la originea numelui său.',
             'Prezintă, în 30-50 de cuvinte, atmosfera din sala „Odobescu”, de dinaintea începerii cursului susținut de Vasile Pârvan, așa cum reiese din textul dat. ',
         ],
-        punctaj: [5, 5, 5, 5, 5]
+        punctaj: [6, 6, 6, 6, 6]
     },
     {
         titlu: 'Redactează un text ',
@@ -86,13 +86,36 @@ Căci în fața noastră, a studenților săi extaziați de maestrul lor, nu vor
         tip: 'compozitie'
     },
     {
-        titlu: 'Compoziția pe o temă dată',
-        descriere: 'Scrie o compoziție pe o temă dată, folosind exemple din literatura română și universală. Demonstrează cunoștințele literare și capacitatea de argumentare.',
+        titlu: 'Prezentarea unui fragment literar',
+        descriere: 'Prezintă, în minimum 50 de cuvinte, rolul notațiilor autorului în fragmentul de mai jos. ',
         numarSubiect: 2,
         profil: 'real',
-        data: '2024',
-        an: 2024,
-        tip: 'compozitie'
+        data: '2025',
+        an: 2025,
+        tip: 'eseu',
+        text: `ACTUL I 
+        TABLOUL I 
+        Încăpere strâmtă, săracă, dar curată, în casa curelarului Ion Sorcovă.  
+        În mijloc, ușă deschisă: se văd poarta și strada, iar dincolo de stradă, maidan de gunoi, tăiat în două de linia ferată. În dreapta ușii, fereastră cu perdele albe și mușcate roșii. În stânga ușii, un dulap de lemn. În colț, pat de scânduri [...]. 
+        E în amurg și ultimele raze ale soarelui împurpurează perdelele albe. [...] 
+        SCENA II 
+        SORCOVĂ, NASTASIA, VULPAȘIN 
+        VULPAȘIN (s-a oprit în prag, sfielnic, se descoperă; e în cămașă, cu mânecile sumese și palmele murdare, așa cum a ieșit din atelier; dând cu ochii de Nastasia, tresare și coboară privirea): Bună seara! (Nu știe ce să facă cu pălăria.) 
+        NASTASIA (s-a întunecat, nemulțumire). 
+        SORCOVĂ: Noroc, Vulpașine! 
+        VULPAȘIN (încearcă surâs și se apropie de Nastasia cu mâna întinsă): Îmi pare bine că te văd! 
+        NASTASIA (îi întoarce spatele și iese, trântind ușa). 
+        G.M. Zamfirescu, Domnișoara Nastasia  `,
+        cerinte: [
+            `Prezintă, în minimum 50 de cuvinte, rolul notațiilor autorului în fragmentul de mai jos. `,
+            `Conținut: precizează rolul notațiilor autorului în fragmentul de mai jos`,
+            `Redactare: utiliză corect conectorii în argumentare; respectă normele limbii literare (exprimare, ortografie, punctuație), așezarea în pagină și lizibilitatea`,
+        ],
+        punctaj: [
+            `Total: 10`,
+            `Conținut: 6(precizează rolul notațiilor autorului în fragmentul de mai jos)`,
+            `Redactare: 4(utilizarea limbii literare – 1 punct; logica înlănțuirii ideilor – 1 punct; ortografia – 1 punct; punctuaţia – 1 punct)`,
+        ]
     },
     {
         titlu: 'Analiza comparativă',
@@ -146,24 +169,46 @@ Căci în fața noastră, a studenților săi extaziați de maestrul lor, nu vor
         punctaj: [6, 6, 6]
     },
     {
-        titlu: 'Analiza unui text literar',
-        descriere: 'Analizează un text literar din perspectiva tematică, stilistică și a compoziției. Identifică elementele specifice genului literar și argumentează cu exemple din text.',
-        numarSubiect: 1,
-        subpunct: 'B',
-        profil: 'real',
-        data: '2023',
-        an: 2023,
-        tip: 'analiza'
-    },
-    {
-        titlu: 'Analiza unui text literar',
-        descriere: 'Analizează un text literar din perspectiva tematică, stilistică și a compoziției. Identifică elementele specifice genului literar și argumentează cu exemple din text.',
+        titlu: 'Gabriel Dimisianu, O vizită la Tudor Arghezi, în vol. Amintiri și portrete literare',
+        descriere: 'Citeşte următorul fragment al autorului Gabriel Dimisianu, O vizită la Tudor Arghezi, în vol. Amintiri și portrete literare și rezolvă următoarele cerinţe:',
         numarSubiect: 1,
         subpunct: 'A',
         profil: 'real',
-        data: '2023',
-        an: 2023,
-        tip: 'analiza'
+        data: '2024',
+        an: 2024,
+        tip: 'analiza',
+        text: `Eram, în 1960, tânăr redactor la Gazeta literară, când colega mea Andriana Fianu mi-a propus pe neașteptate, într-o după-amiază, s-o însoțesc acasă la Tudor Arghezi, de unde urma să luăm un text pe care poetul îl pregătise pentru revistă. [...] 
+        I-am fost recunoscător Andrianei Fianu pentru ideea de-a mă lua cu ea la Arghezi, asta în primul moment, după care m-au copleșit emoțiile și mai c-aș fi renunțat. Atât că, ghicindu-mi frământarea, energica Adi nu mi-a lăsat timp să mă răzgândesc, ci m-a luat scurt: „Hai că nu te mănâncă, grăbește-te că-ntârziem!”. Ce era să fac? Am urmat-o fără murmur, pe drum tot gândindu-mă cum să mă comport când mă voi afla în fața marelui om. Voi fi ochi și urechi, dar mut, am decis până la urmă. Voi fi însoțitorul mut al colegei mele, care, prietenă cu Mițura*, obișnuită a casei, va avea ea toată inițiativa. Înainte de-a ne urca în troleibuz, în Piața Romană, am cumpărat flori, căci ne aflam în preajma zilei de 21 mai, când era aniversarea lui Arghezi. Și nu o aniversare oarecare, poetul împlinind chiar atunci optzeci de ani. 
+        În epoca de care vorbesc, familia Arghezi ocupa un apartament la parterul unui mic bloc de pe Bulevardul Aviatorilor, în vecinătatea imediată a pieței cu același nume, azi Piața Charles de Gaulle. Am sunat, cineva ne-a deschis, conducându-ne într-un living cu ferestre mari, unde, după ce am așteptat câteva minute, a intrat poetul, neînsoțit. Ce m-a izbit îndată au fost pașii repezi făcuți de Arghezi, siguranța mișcărilor de om în toată puterea. Imaginea contrasta frapant cu aceea a neajutorării fizice pe care-o aveam despre poet de la cele câteva apariții în săli publice. Acolo pășea încet, ezitant, sprijinit în baston și ținut întotdeauna de braț de atleticul Baruțu*. Aici, în schimb, în spațiul intim, Arghezi se mișca dezinvolt, fără baston și fără Baruțu. Peste puțin timp a intrat și doamna Paraschiva, care s-a așezat pe un fotoliu, alături de Arghezi. [...]  
+        De ce mi-era teamă, n-am scăpat. Adi m-a prezentat, cum era firesc, din moment ce mă adusese cu ea, dar a adăugat, rea inspirație, că sunt critic literar, unul dintre criticii tineri [...] în care se pun speranțe etc. Arghezi a reacționat, săgetându-mă cu o întrebare care m-a descumpănit: „Și ce vrei dumneata să critici?”. M-am pierdut, cred că roșisem, după care totuși am încercat să articulez un răspuns care era mai degrabă o dezvinovățire. Că eu de fapt nu vreau să critic ceva sau pe cineva, că mai mult decât să scriu îmi place să citesc, că dacă am o pasiune este cititul, însă, din obligație redacțională, trebuie din când în când să fac prezentări de cărți, nu neapărat critice… „Așa mai merge”, mi-a oprit Arghezi chinuita perorație*, schițând parcă și un zâmbet, după care m-a lăsat în plata Domnului, nemaiîntrebându-mă nimic. Fapt este că în acea împrejurare, luat repede și țintuit de privirea ironică a poetului, m-am lepădat nu ca Petru, de trei ori, ci numai o dată, de profesiunea pe care tocmai începusem s-o practic, de bine, de rău. Nu am mințit totuși în ce privește plăcerea cititului, care o întrecea, cum o întrece și acum, pe aceea a scrisului.  `,
+        cerinte: [
+            'Indică sensul din text al secvenței pe neașteptate și al cuvântului împrejurare. ',
+            'Menționează numele revistei la care lucrează Andriana Fianu, utilizând informaţiile din textul dat.',
+            'Precizează modul în care Gabriel Dimisianu își propune să se comporte în timpul vizitei la Tudor Arghezi, justificându-ți răspunsul cu o secvență semnificativă din textul dat. ',
+            'Explică motivul pentru care Gabriel Dimisianu este surprins când Tudor Arghezi intră în cameră.',
+            'Prezintă, în 30-50 de cuvinte, o trăsătură morală a lui Gabriel Dimisianu, care se desprinde din textul dat. ',
+        ],
+        punctaj: [6, 6, 6, 6, 6]
+    },
+    {
+        titlu: 'Redactează un text ',
+        descriere: 'Redactează un text de minimum 150 de cuvinte, în care să argumentezi dacă emoțiile influențează sau nu sinceritatea exprimării. ',
+        numarSubiect: 1,
+        subpunct: 'B',
+        profil: 'real',
+        data: '2024',
+        an: 2024,
+        tip: 'redactare',
+        text: `Eram, în 1960, tânăr redactor la Gazeta literară, când colega mea Andriana Fianu mi-a propus pe neașteptate, într-o după-amiază, s-o însoțesc acasă la Tudor Arghezi, de unde urma să luăm un text pe care poetul îl pregătise pentru revistă. [...] 
+        I-am fost recunoscător Andrianei Fianu pentru ideea de-a mă lua cu ea la Arghezi, asta în primul moment, după care m-au copleșit emoțiile și mai c-aș fi renunțat. Atât că, ghicindu-mi frământarea, energica Adi nu mi-a lăsat timp să mă răzgândesc, ci m-a luat scurt: „Hai că nu te mănâncă, grăbește-te că-ntârziem!”. Ce era să fac? Am urmat-o fără murmur, pe drum tot gândindu-mă cum să mă comport când mă voi afla în fața marelui om. Voi fi ochi și urechi, dar mut, am decis până la urmă. Voi fi însoțitorul mut al colegei mele, care, prietenă cu Mițura*, obișnuită a casei, va avea ea toată inițiativa. Înainte de-a ne urca în troleibuz, în Piața Romană, am cumpărat flori, căci ne aflam în preajma zilei de 21 mai, când era aniversarea lui Arghezi. Și nu o aniversare oarecare, poetul împlinind chiar atunci optzeci de ani. 
+        În epoca de care vorbesc, familia Arghezi ocupa un apartament la parterul unui mic bloc de pe Bulevardul Aviatorilor, în vecinătatea imediată a pieței cu același nume, azi Piața Charles de Gaulle. Am sunat, cineva ne-a deschis, conducându-ne într-un living cu ferestre mari, unde, după ce am așteptat câteva minute, a intrat poetul, neînsoțit. Ce m-a izbit îndată au fost pașii repezi făcuți de Arghezi, siguranța mișcărilor de om în toată puterea. Imaginea contrasta frapant cu aceea a neajutorării fizice pe care-o aveam despre poet de la cele câteva apariții în săli publice. Acolo pășea încet, ezitant, sprijinit în baston și ținut întotdeauna de braț de atleticul Baruțu*. Aici, în schimb, în spațiul intim, Arghezi se mișca dezinvolt, fără baston și fără Baruțu. Peste puțin timp a intrat și doamna Paraschiva, care s-a așezat pe un fotoliu, alături de Arghezi. [...]  
+        De ce mi-era teamă, n-am scăpat. Adi m-a prezentat, cum era firesc, din moment ce mă adusese cu ea, dar a adăugat, rea inspirație, că sunt critic literar, unul dintre criticii tineri [...] în care se pun speranțe etc. Arghezi a reacționat, săgetându-mă cu o întrebare care m-a descumpănit: „Și ce vrei dumneata să critici?”. M-am pierdut, cred că roșisem, după care totuși am încercat să articulez un răspuns care era mai degrabă o dezvinovățire. Că eu de fapt nu vreau să critic ceva sau pe cineva, că mai mult decât să scriu îmi place să citesc, că dacă am o pasiune este cititul, însă, din obligație redacțională, trebuie din când în când să fac prezentări de cărți, nu neapărat critice… „Așa mai merge”, mi-a oprit Arghezi chinuita perorație*, schițând parcă și un zâmbet, după care m-a lăsat în plata Domnului, nemaiîntrebându-mă nimic. Fapt este că în acea împrejurare, luat repede și țintuit de privirea ironică a poetului, m-am lepădat nu ca Petru, de trei ori, ci numai o dată, de profesiunea pe care tocmai începusem s-o practic, de bine, de rău. Nu am mințit totuși în ce privește plăcerea cititului, care o întrecea, cum o întrece și acum, pe aceea a scrisului.  `,
+        cerinte: [
+            'Cerințe totale: Redactează un text de minimum 150 de cuvinte, în care să argumentezi dacă emoțiile influențează sau nu sinceritatea exprimării, raportându-te atât la informațiile din fragmentul extras din volumul Amintiri și portrete literare de Gabriel Dimisianu, cât și la experiența personală sau culturală.  ',
+            'Sumar conținut:  formularea unei opinii faţă de problematica pusă în discuţie, enunţarea şi dezvoltarea corespunzătoare a două argumente adecvate opiniei și formularea unei concluzii pertinente',
+            'Sumar redactare:  utilizarea corectă a conectorilor în argumentare, respectarea normelor limbii literare (norme de exprimare, de ortografie și de punctuație), aşezarea în pagină, lizibilitatea, respectarea precizării privind numărul minim de cuvinte. '
+        ],
+        punctaj: ['Total: 20', 'Sumar conținut: 14', 'Sumar redactare: 6']
     },
     {
         titlu: 'Analiza unui text literar',
@@ -185,13 +230,31 @@ Căci în fața noastră, a studenților săi extaziați de maestrul lor, nu vor
         tip: 'compozitie'
     },
     {
-        titlu: 'Compoziția pe o temă dată',
-        descriere: 'Scrie o compoziție pe o temă dată, folosind exemple din literatura română și universală. Demonstrează cunoștințele literare și capacitatea de argumentare.',
+        titlu: 'Prezentarea unui fragment literar',
+        descriere: 'Prezintă, în minimum 50 de cuvinte, rolul notațiilor autorului în fragmentul de mai jos. ',
         numarSubiect: 2,
         profil: 'real',
-        data: '2023',
-        an: 2023,
-        tip: 'compozitie'
+        data: '2024',
+        an: 2024,
+        tip: 'eseu',
+        text: `ACTUL I 
+        O cameră simplu mobilată. [...] În stânga, lângă perete, o etajeră cu volume legate frumos și frumos orânduite. În dosul etajerei, o ușă care duce în odaia de dormit a doamnei Chirică și în camera copiilor. În dreapta, planul întâi, camera domnului Chirică. O ușă laterală, ceva mai sus, răspunde în coridorul care duce la bucătărie. În fund, un mic vestibul cu geamlâc. 
+        Nichita și Varlam se zăresc în geamlâcul vestibulului. Varlam deschide și îi face loc lui Nichita. Amândoi își agață pălăriile în cuier. 
+        NICHITA: Sunt tare curios să-l văd la față. 
+        VARLAM: E neschimbat ca înfățișare, atâta doar, că anii i-au cam pungit obrajii. (Oprindu-se în prag.) Să știi că nu-i acasă. 
+        NICHITA: Ce te face să crezi? 
+        VARLAM: Dacă nu-i în colțișorul lui (arată biroul), slabă nădejde să fie... (Deschide ușa din dreapta, dispare o clipă, apoi se întoarce.) Nu-i. 
+        Gh. Ciprian, Omul cu mârțoaga `,
+        cerinte: [
+            `Prezintă, în minimum 50 de cuvinte, rolul notațiilor autorului în fragmentul de mai jos. `,
+            `Conținut: precizează rolul notațiilor autorului în fragmentul de mai jos`,
+            `Redactare: utiliză corect conectorii în argumentare; respectă normele limbii literare (exprimare, ortografie, punctuație), așezarea în pagină și lizibilitatea`,
+        ],
+        punctaj: [
+            `Total: 10`,
+            `Conținut: 6(precizează rolul notațiilor autorului în fragmentul de mai jos)`,
+            `Redactare: 4(utilizarea limbii literare – 1 punct; logica înlănțuirii ideilor – 1 punct; ortografia – 1 punct; punctuaţia – 1 punct)`,
+        ]
     },
     {
         titlu: 'Analiza comparativă',
@@ -203,13 +266,20 @@ Căci în fața noastră, a studenților săi extaziați de maestrul lor, nu vor
         tip: 'comparativ'
     },
     {
-        titlu: 'Analiza comparativă',
-        descriere: 'Realizează o analiză comparativă între două opere literare, evidențiind asemănările și deosebirile din perspectiva tematică, stilistică și a compoziției.',
+        titlu: 'Redactare eseu',
+        descriere: 'Redactează un eseu de minimum 400 de cuvinte, în care să prezinți particularități ale unei nuvele studiate. ',
         numarSubiect: 3,
         profil: 'real',
-        data: '2023',
-        an: 2023,
-        tip: 'comparativ'
+        data: '2024',
+        an: 2024,
+        tip: 'eseu',
+        text: `Redactează un eseu de minimum 400 de cuvinte, în care să prezinți particularități de construcție a unui personaj într-un text narativ studiat, aparținând lui Ion Creangă sau lui Ioan Slavici. În elaborarea eseului, vei avea în vedere următoarele repere: – evidențierea a două trăsături care fac posibilă încadrarea nuvelei studiate într-o perioadă, într-un curent cultural/literar sau într-o orientare tematică; – comentarea a două episoade/secvențe semnificative pentru tema nuvelei studiate; – analiza a două elemente de structură, de compoziție și/sau de limbaj, relevante pentru nuvela studiată (de exemplu: acțiune, conflict, relații temporale și spațiale, incipit, final, tehnici narative, instanțe ale comunicării narative, perspectivă narativă, registre stilistice, limbaj etc.). `,
+        cerinte: [
+            '',
+            'Evidențiază o trăsătură a personajului ales, prin două episoade/secvențe comentate',
+            'Analizează două elemente de structură, compoziție și/sau limbaj relevante pentru construcția personajului'
+        ],
+        punctaj: [6, 6, 6]
     },
     {
         titlu: 'Analiza unui text literar',
